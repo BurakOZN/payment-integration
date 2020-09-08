eda2dc6c85f40201eab2c6161cbf0f4625ba12e0
bozen <USR@DESKTOP-D1G942V>
2020-09-08 18:23:58 +0300

    Added property. 3D auth/pre-auth.

--- PaymentIntegration/Model/PreAuth3DResponse.cs
+++ PaymentIntegration/Model/PreAuth3DResponse.cs
@@ -5,6 +5,7 @@
         public string HtmlContent { get; set; }
         public string ProcessId { get; set; }
         public string OrderId { get; set; }
+        public string ResultCode { get; set; }
+        public string ResultMessage { get; set; }
     }
-
 }
